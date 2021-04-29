06cdebd2d6a96bbefbe8302c00de8e9435ef5e7e
dobedh <dobedh@gmail.com>
2021-04-29 23:22:14 +0900

    twitter coding firebase setup_v2

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAv_9yPx5dbvJ0rBSY7r-jPiTzNIwzXSyQ",
+  authDomain: "nwiter-15745.firebaseapp.com",
+  projectId: "nwiter-15745",
+  storageBucket: "nwiter-15745.appspot.com",
+  messagingSenderId: "976518676806",
+  appId: "1:976518676806:web:f1fb45a4ad52481871a547",
+};
+
+export default firebase.initializeApp(firebaseConfig);
