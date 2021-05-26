c237a6a89a98578c9c77e26f789031089ffd07df
dobedh <dobedh@gmail.com>
2021-05-26 23:41:06 +0900

    Login basic with useState

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+      console.log(value);
+    } else if (name === "password") {
+      setPassword(value);
+      console.log(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="login" />
+      </form>
+      <button>Login With Facebook</button>
+      <button>Login With Google</button>
+    </div>
+  );
 };
 
 export default Auth;
