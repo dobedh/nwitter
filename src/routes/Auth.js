c5cdfe69d9476516bf165294edc1872fd6cf25e6
dobedh <dobedh@gmail.com>
2021-05-29 23:07:37 +0900

    Social login added_v1

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -30,6 +30,20 @@ const Auth = () => {
       setError(error.message);
     }
   };
+  const onSocialClick = async (event) => {
+    let provider;
+    const {
+      target: { name },
+    } = event;
+    console.log(name);
+    if (name === "facebook") {
+      provider = new firebaseInstance.auth.FacebookAuthProvider();
+    } else if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
   const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
@@ -54,8 +68,12 @@ const Auth = () => {
       <span onClick={toggleAccount}>
         {newAccount ? "Sign In" : "Create Account"}
       </span>
-      <button>Login With Facebook</button>
-      <button>Login With Google</button>
+      <button onClick={onSocialClick} name="facebook">
+        Login With Facebook
+      </button>
+      <button onClick={onSocialClick} name="google">
+        Login With Google
+      </button>
     </div>
   );
 };
