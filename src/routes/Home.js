386093ed4712b5052a1840d32cdc2a46420243b9
dobedh <dobedh@gmail.com>
2021-05-31 00:21:16 +0900

    firestore, get, add, collection, for each

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,20 @@ import { db } from "fbase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const [dbNweets, setDbNweets] = useState("");
+  const [nweets, setNweets] = useState([]);
   const getNeweets = async () => {
-    const nweets = await db.collection("nweets").get();
-    console.log(nweets);
+    const dbNweets = await db.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
   };
   useEffect(() => {
     getNeweets();
-  });
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -43,6 +49,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
