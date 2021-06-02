ab77bef4374b91a5cd4fae7b295c469205088ab3
dobedh <dobedh@gmail.com>
2021-06-02 23:46:40 +0900

    delete and update

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "fbase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+  const onChange = (event) => {};
+  const onDeleteClick = () => {
+    db.doc(`nweets/${nweetObj.id}`).delete();
+  };
   return (
     <div key={nweetObj.id}>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
+        <form>
+          <input value={newNweet} required />
+        </form>
+      ) : (
         <>
-          <button>삭제</button>
-          <button>수정</button>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>삭제</button>
+              <button onClick={toggleEditing}>수정</button>
+            </>
+          )}
         </>
       )}
     </div>
